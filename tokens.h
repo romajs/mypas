cf3ef5d8a54ebfbf824526ef79497e9c731f09c1
romajs <joseromualdoneto@gmail.com>
2013-10-27 01:52:14 -0200

    adicionado MAX_STR_LEN

--- tokens.h
+++ tokens.h
@@ -25,6 +25,7 @@ enum natures {
 #define		MAX_ARGS_SIZE				0x100
 #define   		MAX_INT_DIG             				10	// Número máximo de dígitos de um inteiro
 #define	  	MAX_PRC_DIG		          			17	// Número máximo de dígitos de precisão de ponto flutuante
+#define	  	MAX_STR_LEN		          			0x100	// Número máximo de caracteres de uma string
 
 #define	 	MAX_STACK_SIZE		   		0x100  // Tamanho máximo das pilhas			
 #define		MAX_RECURSION_SIZE   		0x100  // Número máximo de recursões	
