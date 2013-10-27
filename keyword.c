a4f782258bbc0676f2ccfa9769b6444369c51eb6
romajs <joseromualdoneto@gmail.com>
2013-10-27 17:42:08 -0200

    criado enum (keyword) dinamico, static const char[]* sincronizado automaticamente

--- keyword.c
+++ keyword.c
@@ -1,43 +1,12 @@
 #include <string.h>
 #include <keyword.h>
 
-char const *keyword_lexemes[]={
-	"BEGIN",
-	"ATTR",
-	"NOT",
-	"OR",
-	"AND",
-	"DIV",
-	"MOD",
-	"ARRAY",
-	"OF",
-	"BOOLEAN",
-	"DOUBLE",
-	"ELSE",
-	"FUNCTION",
-	"IF",
-	"INTEGER",
-	"PROCEDURE",
-	"PROGRAM",
-	"REAL",
-	"REPEAT",
-	"STRING",
-	"THEN",
-	"UNTIL",
-	"VAR",
-	"WHILE",
-	"DO",
-	"FOR",
-	"TO",
-	"DOWNTO",
-	"END",
-};
-
 keyword iskeyword(char const *query)
 {
 	keyword k;
-	for(k = BEGIN; k < END + 1; k++)
-		if(strcmp(query, keyword_lexemes[k - BEGIN]) == 0)
+	for(k = BEGIN; k < END + 1; k++) {
+		if(strcmp(query, keywords_lexeme[k - BEGIN]) == 0)
       return k;
+  }
 	return 0;
 }
--- keyword.h
+++ keyword.h
@@ -1,34 +1,52 @@
+#ifndef KEYWORD_H
+#define KEYWORD_H
+
+#define FOREACH_ENUM(ENUM) \
+	ENUM(BEGIN = 0x10000) \
+	ENUM(PROGRAM) \
+	ENUM(VAR) \
+	ENUM(ATTR) \
+  ENUM(GEQ) \
+  ENUM(LEQ) \
+  ENUM(NEQ) \
+	ENUM(AND) \
+	ENUM(OR) \
+	ENUM(DIV) \
+	ENUM(MOD) \
+	ENUM(NOT) \
+	ENUM(ARRAY) \
+	ENUM(OF) \
+	ENUM(INTEGER) \
+	ENUM(BOOLEAN) \
+	ENUM(REAL) \
+	ENUM(DOUBLE) \
+	ENUM(STRING) \
+	ENUM(IF) \
+	ENUM(THEN) \
+	ENUM(ELSE) \
+	ENUM(FUNCTION) \
+	ENUM(PROCEDURE) \
+	ENUM(REPEAT) \
+	ENUM(UNTIL) \
+	ENUM(FOR) \
+	ENUM(TO) \
+	ENUM(DOWNTO) \
+	ENUM(DO) \
+	ENUM(WHILE) \
+	ENUM(END) \
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
 enum keywords {
-	BEGIN=0x10000,
-	ATTR,
-	NOT,
-	OR,
-	AND,
-	DIV,
-	MOD,
-	ARRAY,
-	OF,
-	BOOLEAN,
-	DOUBLE,
-	ELSE,
-	FUNCTION,
-	IF,
-	INTEGER,
-	PROCEDURE,
-	PROGRAM,
-	REAL,
-	REPEAT,
-	STRING,
-	THEN,
-	UNTIL,
-	VAR,
-	WHILE,
-	DO,
-	FOR,
-	TO,
-	DOWNTO,
-	END
+  FOREACH_ENUM(GENERATE_ENUM)
 };
+
+static const char *keywords_lexeme[] = {
+  FOREACH_ENUM(GENERATE_STRING)
+};
+
+#endif // KEYWORD_H
 
 typedef enum keywords keyword;
 
