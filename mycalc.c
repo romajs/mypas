e1aa454621c7062a717fc58a8d55e6091f201fd0
romajs <joseromualdoneto@gmail.com>
2013-11-02 20:32:34 -0200

    adicionado validações semânticas em expr

--- mycalc.c
+++ mycalc.c
@@ -49,8 +49,6 @@ isrelop(const token_t token)
 precedence(const token_t token)
 {
 	switch(token) {
-    case NOT:
-      return 2;
     case '*':
     case '/':
     case DIV:
@@ -90,53 +88,58 @@ type(token_t token)
 			return 5;
 		default:
       // ERROR TYPE
-			return -1;
+			return 0;
 	}
 }
 
-int chkType(token_t op)
-{
-  int result = -1;
-  token_t acceptableTypes[5] = {0,0,0,0,0}; // INT, REAL, DBL, BOOL, STR
+typeFactor(int type1, int type2) {
+  if(type1 <= DBL_CTE && type2 <= DBL_CTE)
+    return type1 > type2 ? type1 : type2;
+  if(BOOL_CTE == type1 == type2)
+    return BOOL_CTE;
+  if(STR_CTE == type1 == type2 )
+    return STR_CTE;
+  return 0;
+}
+
+int* semanticSupportedTypes(token_t op) {
+  // Order: INT, REAL, DBL, BOOL, STR, ex: {0,0,0,0,0}
   switch(op) {
-    case NOT: break; // BOOL
-		case '+': break; // INT,REAL,DBL | STR
-		case '-': break; // INT,REAL,DBL
-		case '*': break; // INT,REAL,DBL
-		case '/': break; // INT,REAL,DBL
-    case DIV: break; // INT
-    case MOD: break; // INT,REAL,DBL
-    case AND: break; // BOOL
-    case OR:  break; // BOOL
-    case EQ:  break; // *
-    case NEQ: break; // *
-    case GRT: break; // INT,REAL,DBL
-    case GEQ: break; // INT,REAL,DBL
-    case LSR: break; // INT,REAL,DBL
-    case LEQ: break; // INT,REAL,DBL
+		case '+': return (int[]) {1,1,1,0,1}; // INT,REAL,DBL | STR
+		case '-': return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+		case '*': return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+		case '/': return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+    case DIV: return (int[]) {1,0,0,0,0}; // INT
+    case MOD: return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+    case AND: return (int[]) {0,0,0,1,0}; // BOOL
+    case OR:  return (int[]) {0,0,0,1,0}; // BOOL
+    case EQ:  return (int[]) {1,1,1,1,1}; // *
+    case NEQ: return (int[]) {1,1,1,1,1}; // *
+    case GRT: return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+    case GEQ: return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+    case LSR: return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
+    case LEQ: return (int[]) {1,1,1,0,0}; // INT,REAL,DBL
 	}
-	return result;
 }
 
 // função que calcula o resultado entre duas variávies dado seu operador
 double calc(double x, double y, token_t op) { 
   double result = 0.00;
   switch(op) {
-    case NOT: result = !x; break;               // BOOL
-		case '+': result = x + y; break;            // INT,REAL,DBL | STR
-		case '-': result = x - y; break;            // INT,REAL,DBL
-		case '*': result = x * y; break;            // INT,REAL,DBL
-		case '/': result = x / y; break;            // INT,REAL,DBL
-    case DIV: result = (int)(x / y); break;     // INT
-    case MOD: result = (int)x % (int)y; break;  // INT,REAL,DBL
-    case AND: result = x && y; break;           // BOOL   
-    case OR:  result = x || y; break;           // BOOL
-    case EQ:  result = x == y; break;           // *
-    case NEQ: result = x != y; break;           // *
-    case GRT: result = x > y; break;            // INT,REAL,DBL
-    case GEQ: result = x >= y; break;           // INT,REAL,DBL
-    case LSR: result = x < y; break;            // INT,REAL,DBL
-    case LEQ: result = x <= y; break;           // INT,REAL,DBL
+		case '+': result = x + y; break;
+		case '-': result = x - y; break;
+		case '*': result = x * y; break;
+		case '/': result = x / y; break;
+    case DIV: result = (int)(x / y); break;
+    case MOD: result = (int)x % (int)y; break;
+    case AND: result = x && y; break;
+    case OR:  result = x || y; break;
+    case EQ:  result = x == y; break;
+    case NEQ: result = x != y; break;
+    case GRT: result = x > y; break;
+    case GEQ: result = x >= y; break;
+    case LSR: result = x < y; break;
+    case LEQ: result = x <= y; break;
 	}
 	debug( "(calc) %.2f %c %.2f = %.2f\n", x, op, y, result);
 	return result;
@@ -170,7 +173,11 @@ double getvalue(char const *variable) {
 // função que empilha um 'operando' na pilha de operandos
 void push_operand(token_t token, const char *lexeme) {
 	++sp;
-  operand[sp].value = atof(lexeme);
+  if(token == STR_CTE) {
+     strcpy(operand[sp].lexeme, lexeme);
+  } else {
+    operand[sp].value = atof(lexeme);
+  }
   operand[sp].type = type(token);
 	debug( "(push) operand[%d]: value = %.2f, type = %d\n", sp, 
     operand[sp].value, operand[sp].type);	
@@ -202,15 +209,39 @@ void exec_oper(void) {
 	// deve operar quando:
 	if(opsp > - 1) { // se puder operar
 		while(can_oper()) { // enquanto puder operar			
-			debug("(pop) oper[%d]: symbol = \"%c\", level = %d\n",
+
+      // 1 - verificar quais tipos semânticos o operador suporta
+      int *types = semanticSupportedTypes(oper[opsp].symbol);
+      
+      // 2 - verificar se o tipo dos operandos bate com os suportados pelo operador
+      if(types[operand[sp].type] && types[operand[sp+1].type]) {
+        goto ERROR;
+      }
+      
+      debug("(pop) oper[%d]: symbol = \"%c\", level = %d\n",
         opsp, oper[opsp].symbol, oper[opsp].level);	
-			debug("(pop) operand[%d] = %.2f\n", sp, operand[sp]);
-			debug("(pop) operand[%d] = %.2f\n", sp-1, operand[sp-1]);	
-			operand[--sp].value = calc(operand[sp].value, operand[sp+1].value,
-        oper[opsp--].symbol);
+      debug("(pop) operand[%d] = %.2f\n", sp, operand[sp]);
+      debug("(pop) operand[%d] = %.2f\n", sp-1, operand[sp-1]);	
+      
+      // 3 - verificar se os tipos entre os operandos batem
+      int resultType = typeFactor(operand[sp].type, operand[sp+1].type);
+      if(resultType) {          
+        goto ERROR;
+      }
+      
+      // 4 - fazer o cáculo da operação
+      operand[--sp].value =
+        calc(operand[sp].value, operand[sp+1].value, oper[opsp--].symbol); 
+        
+      // 5 - determinar tipo do resultado final da operação (do operando)        
+      operand[--sp].type = resultType;
+      
+      // TODO: se termiar o nível deve decrementá-lo
 		} 
-	} 
-	//debug_oper(oper, E_lvl, opsp);
+	}
+ERROR:
+  err(FATAL, SEMANTIC, "Incorret semantic operation.\n");
+  // TODO:
 }
 
 double expr(void)
@@ -223,11 +254,14 @@ double expr(void)
 
 	E: debug( "E: %d\n", ++E_lvl);
 
-	if(lookahead == '-') { // inversão de sinal
-		match('-');
-		debug( "(signal reversion) activated.\n");
-		//push_operand(0);
-		//push_oper('-');
+	switch(lookahead) { // inversão de sinal ('-') e negação (NOT)
+    case '-':
+    case NOT:
+      debug( "(signal reversion) activated.\n");
+      //push_operand(lookahead, 0.00);
+      //push_oper(lookahead, E_lvl);
+      match(lookahead);
+      break;
 	}
 
 	T: debug( "T: %d\n", ++T_lvl);	
@@ -259,22 +293,19 @@ double expr(void)
      *** Constants defs goes next: INTEGER | REAL | DOUBLE | BOOLEAN | STRING *
      *************************************************************************/
     case TRUE: 
-      //push_operand(1.00);
+      //push_operand(TRUE, "1");
       match(TRUE);
       break;
     case FALSE:
-      //push_operand(0.00);
+      //push_operand(FALSE, "0");
       match(FALSE);
       break;
     case INT_CTE:
     case REAL_CTE:
     case DBL_CTE:
-      //push_operand(atof(lexeme)); // empilha o valor da constante
-      match(lookahead);
-      break;
     case STR_CTE:
-      // TODO: push_operand(???);
-      match(STR_CTE);
+      // push_operand(lookahead, lexeme);
+      match(lookahead);
       break;
     /**************************************************************************
      *** End of Constants defitions *******************************************
